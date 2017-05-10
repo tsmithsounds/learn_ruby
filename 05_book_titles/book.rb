69cf34fd3da05880eba97cbffd4dd2db4705f84d
Tony Smith <tsmith.sounds@gmail.com>
2017-05-10 15:14:13 -0700

    Solved book title exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+
+	attr_reader :title
+
+	def title=(new_title)
+		lower_case = %w(and the a an in of)
+		words = new_title.split.map { |word| 
+			
+			if lower_case.include? word
+				word.downcase
+			else 
+				word.capitalize
+			end
+		}
+		words[0].capitalize!
+		@title = words.join(" ")
+		
+	end
 end
+
